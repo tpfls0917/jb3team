1f0b6f91254cc609452424d285d690e6e5a1c743
ryusunwoo <72438634+ryusunwoo@users.noreply.github.com>
2020-11-13 14:19:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 // main.js
 
-// 2단계 메뉴
+  // gnb
 $(function(){
-    $('.depth1 > li').hover(
+  $('#gnb > .depth1 > li').hover(
       function(){
-        $('.depth2').stop().slideDown(200);
+          $('.depth2').stop().show();
+          $('#gnb_bg').addClass('show');
       },
       function(){
-        $('.depth2').stop().slideUp(200);
+          $('.depth2').stop().hide();
+          $('#gnb_bg').removeClass('show');
       }
-    )
-  })
+  )
+});
+
 
   // bxslider 
 $(document).ready(function(){
